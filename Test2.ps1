5d86370cadbf35331fa921bd1edf1b3f205fbeb4
alfredoreyesstk <53196959+alfredoreyesstk@users.noreply.github.com>
2020-01-09 12:29:49 -0600

    Update Test2.ps1

--- Test2.ps1
+++ Test2.ps1
@@ -1,14 +1,4 @@
 Write-Host "Obtener los xmls de Git"
 Write-Host "Cuantos xmls hay?"
-$rutaXML = "$(System.DefaultWorkingDirectory)"
-
-$lista_xml = get-childitem -path $rutaXML  -filter "*.xml"
-
-$numArchivos = 0
-
-foreach ($row in $lista_xml)
-{
-	$xml = $rutaXML + $row
-	 Write-Host "Leyendo Archivo - "  $numArchivos $xml
-}
+Write-Host $Env:BUILD_SOURCESDIRECTORY
 Write-Host "Fin del proceso"
