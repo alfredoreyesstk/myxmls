448aa9062bad5355cbf96ed1da88ca285c906264
alfredoreyesstk <53196959+alfredoreyesstk@users.noreply.github.com>
2020-01-09 12:33:40 -0600

    probemos el for

--- Test2.ps1
+++ Test2.ps1
@@ -2,4 +2,14 @@ Write-Host "Obtener los xmls de Git"
 Write-Host "Cuantos xmls hay?"
 $rutaXML = $Env:BUILD_SOURCESDIRECTORY
 Write-Host $rutaXML
+
+$lista_xml = get-childitem -path $rutaXML  -filter "*.xml"
+
+$numArchivos = 0
+
+foreach ($row in $lista_xml)
+{
+	$xml = $rutaXML + $row
+	 Write-Host "Leyendo Archivo - "  $numArchivos $xml
+}
 Write-Host "Fin del proceso"
